25010c194e7e14af3e82223471b46ce0a033b608
danluan <danielluanlourencol@gmail.com>
2025-09-06 15:02:37 -0300

    inicio do projeto

---
+++ service-link-backend/src/main/java/br/com/servicelink/ServiceLinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.servicelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLinkApplication.class, args);
+	}
+
+}
