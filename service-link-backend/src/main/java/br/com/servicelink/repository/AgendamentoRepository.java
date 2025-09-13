e3bb81bc7fba28fddae371b72215481ea5ca1cc3
pulisaglio <pulisaglio@gmail.com>
2025-09-13 18:46:58 -0300

    Fix: Nome de funçao de Agendamento Repository mudado de findByData para findByDataHora

--- service-link-backend/src/main/java/br/com/servicelink/repository/AgendamentoRepository.java
+++ service-link-backend/src/main/java/br/com/servicelink/repository/AgendamentoRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-    List<Agendamento> findByData(LocalDate data);
+    List<Agendamento> findByDataHora(LocalDate data);
 }
